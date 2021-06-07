24d6376a2afb3bfbe56b51b145501d05bdcb0d9b
H-SChan <dlwp7581@gmail.com>
2021-06-07 12:03:59 +0900

    [add] delete banner

--- src/main/java/gg/jominsubyungsin/admin/domain/repository/BannerListRepo.java
+++ src/main/java/gg/jominsubyungsin/admin/domain/repository/BannerListRepo.java
@@ -3,5 +3,5 @@ package gg.jominsubyungsin.admin.domain.repository;
 import gg.jominsubyungsin.domain.entity.BannerEntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface BannerListRepository extends PagingAndSortingRepository<BannerEntity, Long> {
+public interface BannerListRepo extends PagingAndSortingRepository<BannerEntity, Long> {
 }
